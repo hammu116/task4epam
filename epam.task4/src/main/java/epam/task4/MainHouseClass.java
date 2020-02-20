e224c760fd8265aeac5a88709edd0f8fa94f9970
hammu116 <59144819+hammu116@users.noreply.github.com>
2020-02-20 22:29:10 +0530

    Add files via upload

---
+++ epam.task4/src/main/java/epam/task4/MainHouseClass.java
@@ -0,0 +1,13 @@
+package epam.task4;
+import java.io.IOException;
+
+public class MainHouseClass 
+	{
+		    public static void main(String args[]) throws IOException 
+		    {
+		    	HouseConstruction c = new HouseConstruction();
+		    	System.out.println("Cost of Construction of House");
+		    	System.out.println("-----------------------------");
+		    	c.costEstimation();
+		    }
+	}
